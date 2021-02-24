4289e9aab214e21c236cee0b84ce01e84c3bc987
Devinawesley <Devinawesley@gmail.com>
2021-02-23 20:47:50 -0500

    Removed print lines
    Removing some prints that were being used for debugging. Whoops!

--- StockWatch.py
+++ StockWatch.py
@@ -16,11 +16,9 @@ def WatchPage(itemURL, twilioNumber, textToNumber, driverPath, firefoxLocation, 
     outOfStock = True
     while outOfStock == True:
         driver.get(itemURL)
-        print("got url")
         if driver.find_elements_by_class_name(elementClassName):
             SendText(itemURL, twilioNumber, textToNumber, client)
             outOfStock = False
         else:
-            print("else")
             print(str(datetime.now()) + " Not in stock.")
             time.sleep(5)
