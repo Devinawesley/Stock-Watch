c98cd5876c33bddf0ab46ff1a264e3d8db292dce
Devinawesley <Devinawesley@gmail.com>
2021-02-23 20:09:44 -0500

    Abstraction, re-usability, and threading
    Moved the main functions to separate module. Added flexibility to watch stock on any website where the "add to cart" or equivalent button is target-able via class name. Added threading to support watching stock on multiple pages.

---
+++ StockChecker.py
@@ -0,0 +1,45 @@
+from selenium.webdriver.firefox.options import Options
+from threading import Thread
+import concurrent.futures
+from itertools import repeat
+import StockWatch
+from twilio.rest import Client
+
+#list of strings of full URLs of each item to watch
+itemURLs = [""]
+
+#API key and secret provided by twilio
+twilioKey = ""
+twilioSecret = ""
+client = Client(twilioKey, twilioSecret)
+
+#phone numbers as strings in format "+55555555555"
+twilioNumber = ""
+textToNumber = ""
+
+#full path to geckodriver.exe
+driverPath = r""
+
+#full path to firefox.exe
+firefoxLocation = Options()
+firefoxLocation.binary_location = r""
+
+#class name for add to cart button on www.microcenter.com is "STBTN". www.walmart.com uses "spin-button-children"
+#right click > inspect element on the web page. Dig around until you find the right class name for the website.
+elementClassName = ""
+
+if __name__ == '__main__':
+    with concurrent.futures.ThreadPoolExecutor(max_workers=len(itemURLs)) as executor:
+        executor.map(
+        StockWatch.WatchPage, 
+        itemURLs, 
+        repeat(twilioNumber), 
+        repeat(textToNumber), 
+        repeat(driverPath), 
+        repeat(firefoxLocation),
+        repeat(elementClassName),
+        repeat(client))
+
+
+
+        
---
+++ StockWatch.py
@@ -0,0 +1,26 @@
+from selenium.webdriver.firefox import webelement
+from twilio.rest import Client
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from datetime import datetime
+import time
+
+def SendText(itemURL, twilioNumber, textToNumber, client):
+    client.messages.create(to=textToNumber,
+                        from_=twilioNumber,
+                        body="IN STOCK! " + itemURL)
+    print(str(datetime.now()) + " In stock, text sent to " + textToNumber)
+
+def WatchPage(itemURL, twilioNumber, textToNumber, driverPath, firefoxLocation, elementClassName, client):
+    driver = webdriver.Firefox(options=firefoxLocation, executable_path=driverPath)
+    outOfStock = True
+    while outOfStock == True:
+        driver.get(itemURL)
+        print("got url")
+        if driver.find_elements_by_class_name(elementClassName):
+            SendText(itemURL, twilioNumber, textToNumber, client)
+            outOfStock = False
+        else:
+            print("else")
+            print(str(datetime.now()) + " Not in stock.")
+            time.sleep(5)
